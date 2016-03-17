630506768d1c7b5a30f2cb1e82603d0abb4ee376
Gavin <gavin@tryingx.com>
2016-03-17 23:42:11 +0800

    First Push Project

---
+++ DGProjectDemo/DGClasses/DGMain/DGBaseViewController/DGBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  DGBaseViewController.h
+//  DGProjectDemo
+//
+//  Created by Gavin on 16/3/17.
+//  Copyright © 2016年 com.tryingx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGBaseViewController : UIViewController
+
+@end
---
+++ DGProjectDemo/DGClasses/DGMain/DGMainViewController/DGMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DGMainViewController.h
+//  DGProjectDemo
+//
+//  Created by Gavin on 16/3/17.
+//  Copyright © 2016年 com.tryingx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DGMainViewController : UITabBarController
+
+@end
